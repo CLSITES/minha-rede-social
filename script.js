04c67319a58918466c3c38cab33e763bd472c93f
SITE <lucasdbr15@gmail.com>
2025-12-19 09:51:53 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,54 @@
+const firebaseConfig = {
+  apiKey: "COLE_AQUI",
+  authDomain: "COLE_AQUI",
+  projectId: "COLE_AQUI",
+  storageBucket: "COLE_AQUI",
+  messagingSenderId: "COLE_AQUI",
+  appId: "COLE_AQUI"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+function register() {
+  auth.createUserWithEmailAndPassword(
+    email.value, password.value
+  );
+}
+
+function login() {
+  auth.signInWithEmailAndPassword(
+    email.value, password.value
+  );
+}
+
+auth.onAuthStateChanged(user => {
+  if (user) {
+    auth.style.display = "none";
+    app.style.display = "block";
+    loadFeed();
+  }
+});
+
+function postImage() {
+  const file = image.files[0];
+  const ref = storage.ref("posts/" + Date.now());
+  ref.put(file).then(() => {
+    ref.getDownloadURL().then(url => {
+      db.collection("posts").add({ image: url });
+      loadFeed();
+    });
+  });
+}
+
+function loadFeed() {
+  feed.innerHTML = "";
+  db.collection("posts").get().then(snapshot => {
+    snapshot.forEach(doc => {
+      feed.innerHTML += `<img src="${doc.data().image}">`;
+    });
+  });
+}
